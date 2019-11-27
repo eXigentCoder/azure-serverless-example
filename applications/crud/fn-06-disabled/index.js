38b74a0679acf42dd95096f5caf631c85dbc7ba8
Ryan Kotzen <potz666@gmail.com>
2019-11-27 12:33:39 +0200

    Updating example app

--- applications/crud/fn-06-disabled/index.js
+++ applications/crud/fn-06-disabled/index.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = async function() {
-    return "Shouldn't see this!";
+    return "Shouldn't see this! ... but you will because the property is deprecated, see https://docs.microsoft.com/en-us/azure/azure-functions/disable-function#functions-2x---all-languages";
 };
